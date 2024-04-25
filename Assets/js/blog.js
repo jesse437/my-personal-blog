cbe437dee1f36ce099138eff1d7c082c2b455b63
Jesus Ruiz Gutierrez <jesse432007@gmail.com>
2024-04-25 16:02:34 -0700

    Added functionality to load data from localstorage to create post on the frontend

--- Assets/js/blog.js
+++ Assets/js/blog.js
@@ -4,3 +4,20 @@ backBtn.addEventListener('click', function(){
     window.location.href = 'index.html'
 })
 
+document.addEventListener('DOMContentLoaded', function(){
+    const posts = JSON.parse(localStorage.getItem('posts')) || [];
+    const blogContainer = document.querySelector('.blog-container');
+    
+    posts.forEach(post => {
+        const postDiv = document.createElement('div');
+        postDiv.classList.add('post-container');
+       postDiv.innerHTML=`
+       <h3>${post.title}</h3>
+       <hr />
+       <div class="content">${post.content}</div>
+       <p>Posted by: ${post.username}</p>
+     `
+     blogContainer.appendChild(postDiv);
+    });
+    
+})
