22dcf4cfb6de1260b4686ba339023ea56c5443a9
Jesus Ruiz Gutierrez <jesse432007@gmail.com>
2024-04-25 09:02:15 -0700

    I more complete verion on my website

---
+++ Assets/js/logic.js
@@ -0,0 +1,25 @@
+let listOfBlogs = [];
+let formEl = document.getElementById("blogForm");
+
+
+function toggleMode(){
+    var body = document.body;
+    var sun = document.getElementById('sun');
+    var moon = document.getElementById('moon');
+
+    if (body.classList.contains('dark-mode')){
+        body.classList.remove('dark-mode');
+        sun.style.display = 'block';
+        moon.style.display='none';
+    }else{
+        body.classList.add('dark-mode');
+        sun.style.display = 'none'
+        moon.style.display='block';
+        body
+    }
+}
+
+formEl.addEventListener("submit", function (event) {
+    event.preventDefault();
+    window.location.href="blog.html"
+});
